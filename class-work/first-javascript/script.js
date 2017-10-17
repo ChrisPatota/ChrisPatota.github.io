7d5c87678efe6b7b71cbe3e476d8a93e1b33be59
ChrisPatota <32045117+ChrisPatota@users.noreply.github.com>
2017-10-16 20:32:43 -0400

    Class 6 HW and Class 7 Classwork

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,14 @@
+var num1 = (10 * 10)
+var num2 = (100 / 10)
+var num3 = (50 - 17)
+var num4 = (50 + 17)
+var name = ("Chris" + " " + "P")
+
+document.write(num1)
+document.write(num2)
+document.write(num3)
+document.write(num4)
+document.write(name)
+
+document.querySelector('#title').style.color = 'red';
+document.querySelector('#text-chunk').style.textDecoration = 'underline';
